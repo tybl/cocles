91f2518da1928579cfd46ae152b54891a4a17c71
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-09-23 09:22:07 -0400

    added category types to sample transaction

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@ extern "C"
 int
 main(int , const char * []) {
    ledger_t ledger;
-   ledger.AddTransaction("{ \"date\":\"2016-09-17\", \"adjustments\":[ {\"account\":\"Toys-R-Us\",\"amount\":77.89}, {\"account\":\"Spending\",\"amount\":-27.17}, {\"account\":\"Birthday\",\"amount\":-50.72}, {\"account\":\"Credit Card\",\"amount\":-77.89} ] }");
+   ledger.AddTransaction("{ "
+      "\"date\":\"2016-09-17\", "
+      "\"adjustments\":[ "
+         "{\"account\":\"Income/Expense::Toys-R-Us\",\"amount\":77.89}, "
+         "{\"account\":\"Budget Category::Spending\",\"amount\":-27.17}, "
+         "{\"account\":\"Budget Category::Birthday\",\"amount\":-50.72}, "
+         "{\"account\":\"Asset/Liability::Credit Card\",\"amount\":-77.89} "
+      "] "
+   "}");
    return 0;
 }
