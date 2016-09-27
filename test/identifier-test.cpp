b06f88e8544d99fd7de0f363c41f34a82ebf0f6f
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-09-27 10:03:27 -0400

    added tests for identifier_t

--- test/identifier-test.cpp
+++ test/identifier-test.cpp
@@ -2,8 +2,32 @@
 
 #include "gtest/gtest.h"
 
-TEST(identifier_t, dogfood) {
-   const unsigned int seed = 123;
-   identifier_t a(seed);
-   EXPECT_EQ(seed, a());
+#include <random>
+
+TEST(identifier_t, binary_dogfood) {
+   std::random_device rd;
+   for (size_t i = 0; i < 10000; ++i) {
+      const unsigned long seed = (static_cast<unsigned long>(rd()) << 32) | rd();
+      identifier_t a(seed);
+      EXPECT_EQ(seed, a());
+   }
+}
+
+TEST(identifier_t, ascii_dogfood) {
+   std::random_device rd;
+   for (size_t i = 0; i < 10000; ++i) {
+      const unsigned long seed = (static_cast<unsigned long>(rd()) << 32) | rd();
+      identifier_t a(seed);
+      EXPECT_EQ(std::to_string(seed), a.ToString());
+   }
+}
+
+TEST(identifier_t, equivalence) {
+   std::random_device rd;
+   for (size_t i = 0; i < 10000; ++i) {
+      const unsigned long seed = (static_cast<unsigned long>(rd()) << 32) | rd();
+      identifier_t a(seed);
+      identifier_t b(seed);
+      EXPECT_EQ(a, b);
+   }
 }
