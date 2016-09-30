35e78b0b4aee592e83ab7bdf748aca83c0acbfc1
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-09-29 23:03:13 -0400

    moved adjustment_t to internal namespace

--- src/ledger/adjustment.cpp
+++ src/ledger/adjustment.cpp
@@ -1,15 +1,17 @@
 #include "adjustment.hpp"
 
 namespace ledger {
-   adjustment_t::adjustment_t(identifier_t new_id,
-                              money_t new_amount,
-                              unsigned long new_account_id,
-                              unsigned long new_transaction_id)
-      : id(new_id),
-      amount(new_amount),
-      account_id(new_account_id),
-      transaction_id(new_transaction_id)
-   {
-      // Intentionally left blank
-   }
+   namespace internal {
+      adjustment_t::adjustment_t(identifier_t new_id,
+                                 money_t new_amount,
+                                 unsigned long new_account_id,
+                                 unsigned long new_transaction_id)
+         : id(new_id),
+         amount(new_amount),
+         account_id(new_account_id),
+         transaction_id(new_transaction_id)
+      {
+         // Intentionally left blank
+      }
+   } // namespace internal
 } // namespace ledger
--- src/ledger/adjustment.hpp
+++ src/ledger/adjustment.hpp
@@ -5,15 +5,17 @@
 #include "money.hpp"
 
 namespace ledger {
-   struct adjustment_t {
-      adjustment_t(identifier_t new_id,
-                   money_t new_amount,
-                   unsigned long new_account_id,
-                   unsigned long new_transaction_id);
-      identifier_t id;
-      money_t amount;
-      identifier_t account_id;
-      identifier_t transaction_id;
-   };
+   namespace internal {
+      struct adjustment_t {
+         adjustment_t(identifier_t new_id,
+                      money_t new_amount,
+                      unsigned long new_account_id,
+                      unsigned long new_transaction_id);
+         identifier_t id;
+         money_t amount;
+         identifier_t account_id;
+         identifier_t transaction_id;
+      }; // struct adjustment_t
+   } // namespace internal
 } // namespace ledger
 #endif // COCLES_LEDGER_ADJUSTMENT_HPP
