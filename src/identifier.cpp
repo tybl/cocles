0cbd3971e170384905f2786a39a3877d6a0d024f
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-09-29 22:46:46 -0400

    moved identifier_t to ledger namespace

--- src/identifier.cpp
+++ src/identifier.cpp
@@ -1,26 +1,28 @@
 #include "identifier.hpp"
 
-identifier_t::identifier_t(unsigned long new_value)
-   : value(new_value)
-{
-}
+namespace ledger {
+   identifier_t::identifier_t(unsigned long new_value)
+      : value(new_value)
+   {
+   }
 
-std::string
-identifier_t::ToString(void) const {
-   return std::to_string(value);
-}
+   std::string
+   identifier_t::ToString(void) const {
+      return std::to_string(value);
+   }
 
-unsigned long
-identifier_t::operator () (void) const {
-   return value;
-}
+   unsigned long
+   identifier_t::operator () (void) const {
+      return value;
+   }
 
-bool
-identifier_t::operator == (const identifier_t &other) const {
-   return value == other.value;
-}
+   bool
+   identifier_t::operator == (const identifier_t &other) const {
+      return value == other.value;
+   }
+} // namespace ledger
 
 std::ostream&
-operator << (std::ostream &out, const identifier_t &id) {
+operator << (std::ostream &out, const ledger::identifier_t &id) {
    return out << id.ToString();
 }
--- src/identifier.hpp
+++ src/identifier.hpp
@@ -4,22 +4,24 @@
 #include <ostream>
 #include <string>
 
-struct identifier_t {
-   identifier_t(unsigned long new_value);
+namespace ledger {
+   struct identifier_t {
+      identifier_t(unsigned long new_value);
 
-   std::string
-   ToString(void) const;
+      std::string
+      ToString(void) const;
 
-   unsigned long
-   operator () (void) const;
+      unsigned long
+      operator () (void) const;
 
-   bool
-   operator == (const identifier_t &other) const;
+      bool
+      operator == (const identifier_t &other) const;
 
-private:
-   unsigned long value;
-};
+   private:
+      unsigned long value;
+   }; // struct identifier_t
+} // namespace ledger
 
-std::ostream& operator << (std::ostream &out, const identifier_t &id);
+std::ostream& operator << (std::ostream &out, const ledger::identifier_t &id);
 
 #endif // COCLES_IDENTIFIER_HPP
