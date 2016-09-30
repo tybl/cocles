7859712a886d2a6a0a8058e123baf6349dd9362d
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-09-29 22:56:45 -0400

    moved money_t to ledger namespace

--- src/money.cpp
+++ src/money.cpp
@@ -2,48 +2,50 @@
 
 #include <iostream>
 
-money_t::money_t(std::string new_value) {
-   mpz_init(value);
-}
+namespace ledger {
+   money_t::money_t(std::string new_value) {
+      mpz_init(value);
+   }
 
-money_t::money_t(double new_value) {
-   mpz_init_set_d(value, new_value * 100);
-}
+   money_t::money_t(double new_value) {
+      mpz_init_set_d(value, new_value * 100);
+   }
 
-money_t::money_t(const money_t &other) {
-   mpz_init_set(value, other.value);
-}
+   money_t::money_t(const money_t &other) {
+      mpz_init_set(value, other.value);
+   }
 
-money_t::~money_t(void) {
-   mpz_clear(value);
-}
+   money_t::~money_t(void) {
+      mpz_clear(value);
+   }
 
-std::string money_t::ToString(void) const {
-   mpz_t whole;
-   mpz_init(whole);
-   unsigned long fraction = mpz_tdiv_q_ui(whole, value, 100);
-   int length = gmp_snprintf(nullptr, 0, "%Zd.%02lu", whole, fraction);
-   length = (0 < length) ? length : 0;
-   std::string result(static_cast<unsigned>(length), '\0');
-   gmp_snprintf(&result[0], length + 1, "%Zd.%02lu", whole, fraction);
-   mpz_clear(whole);
-   return result;
-}
+   std::string money_t::ToString(void) const {
+      mpz_t whole;
+      mpz_init(whole);
+      unsigned long fraction = mpz_tdiv_q_ui(whole, value, 100);
+      int length = gmp_snprintf(nullptr, 0, "%Zd.%02lu", whole, fraction);
+      length = (0 < length) ? length : 0;
+      std::string result(static_cast<unsigned>(length), '\0');
+      gmp_snprintf(&result[0], length + 1, "%Zd.%02lu", whole, fraction);
+      mpz_clear(whole);
+      return result;
+   }
 
-money_t& money_t::operator = (money_t other) {
-   mpz_swap(value, other.value);
-   return *this;
-}
+   money_t& money_t::operator = (money_t other) {
+      mpz_swap(value, other.value);
+      return *this;
+   }
 
-money_t& money_t::operator += (const money_t &other) {
-   mpz_add(value, value, other.value);
-   return *this;
-}
+   money_t& money_t::operator += (const money_t &other) {
+      mpz_add(value, value, other.value);
+      return *this;
+   }
+} // namespace ledger
 
-std::ostream& operator << (std::ostream &out, const money_t &a) {
+std::ostream& operator << (std::ostream &out, const ledger::money_t &a) {
    return out << a.ToString();
 }
 
-money_t operator + (money_t lhs, const money_t &rhs) {
+ledger::money_t operator + (ledger::money_t lhs, const ledger::money_t &rhs) {
    return lhs += rhs;
 }
--- src/money.hpp
+++ src/money.hpp
@@ -5,25 +5,27 @@
 #include <string>
 #include <gmp.h>
 
-struct money_t {
-   money_t(std::string new_value);
+namespace ledger {
+   struct money_t {
+      money_t(std::string new_value);
 
-   money_t(double new_value);
+      money_t(double new_value);
 
-   money_t(const money_t &other);
+      money_t(const money_t &other);
 
-   ~money_t(void);
+      ~money_t(void);
 
-   std::string ToString(void) const;
+      std::string ToString(void) const;
 
-   money_t& operator = (money_t other);
+      money_t& operator = (money_t other);
 
-   money_t& operator += (const money_t &other);
-private:
-   mpz_t value;
-};
+      money_t& operator += (const money_t &other);
+   private:
+      mpz_t value;
+   }; // struct money_t
+} // namespace ledger
 
-std::ostream& operator << (std::ostream &out, const money_t &a);
+std::ostream& operator << (std::ostream &out, const ledger::money_t &a);
 
-money_t operator + (money_t lhs, const money_t &rhs);
+ledger::money_t operator + (ledger::money_t lhs, const ledger::money_t &rhs);
 #endif // COCLES_MONEY_HPP
