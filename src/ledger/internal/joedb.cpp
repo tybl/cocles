f1fc4744bc257ed4760b0deebad72bfeb281ab13
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-10-09 18:34:47 -0400

    Removed unused file

--- src/ledger/internal/joedb.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main () {
-   Database db;
-   auto first_trans = db.new_transaction();
-   db.set_memo(first_trans, "Hello world!");
-   std::cout << db.get_memo(first_trans) << std::endl;
-}
