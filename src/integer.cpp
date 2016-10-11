a0a9304e533e8eaede6131d4d0e4fdabd8fb2fa2
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-10-11 17:08:45 -0400

    WIP: Initialize integer_t with string

--- src/integer.cpp
+++ src/integer.cpp
@@ -3,10 +3,10 @@
 namespace tbl {
 
 integer_t::integer_t(long value)
-   : size(value) {}
+   : mantissa_length(static_cast<size_t>(value * ((0 <= value) ? 1 : -1))) {}
 
 bool integer_t::operator==(const integer_t& other) const {
-   return size == other.size;
+   return mantissa_length == other.mantissa_length;
 }
 
 integer_t& integer_t::operator+=(integer_t ) {
@@ -26,7 +26,7 @@ integer_t& integer_t::operator/=(integer_t ) {
 }
 
 bool integer_t::is_negative() const {
-   return (0 > size);
+   return false;// (0 > size);
 }
 
 } // namespace tbl
@@ -35,4 +35,9 @@ bool integer_t::is_negative() const {
 
 TEST_CASE("tbl::integer_t: integer_t(long), operator==(integer_t)") {
    CHECK(tbl::integer_t(0) == tbl::integer_t(0));
+}
+
+TEST_CASE("tbl::integer_t: integer_t(string)") {
+   tbl::integer_t num("123456789012345678901234567890");
+   CHECK(num.buffer_length == 30);
 }
--- src/integer.hpp
+++ src/integer.hpp
@@ -1,6 +1,9 @@
 #ifndef TBL_INTEGER_HPP
 #define TBL_INTEGER_HPP
 
+#include <cmath>
+#include <iostream>
+#include <limits>
 #include <string>
 
 namespace tbl {
@@ -8,6 +11,25 @@ namespace tbl {
 struct integer_t {
 
    integer_t(long value);
+
+   integer_t(std::string value)
+      : mantissa()
+      , buffer_length(/*log10(strlen(value))*/)
+      , mantissa_length()
+   {
+      // ceil(log10(num)) == num_string.length()
+      // 
+      constexpr double log10_max_uint32 = 9.632959861146281183071;
+      allocate_at_least(static_cast<uint32_t>(std::ceil(value.length() / log10_max_uint32)));
+      for (auto c : value) {
+         if (isdigit(c)) {
+            multiply_mantissa_by_ui(10);
+            add_to_mantissa_at_index_ui(0, static_cast<uint32_t>(c - '0'));
+         } else {
+            break;
+         }
+      }
+   }
 
    bool operator==(const integer_t& other) const;
 
@@ -21,9 +43,51 @@ struct integer_t {
 
    bool is_negative() const;
 
-private:
-   std::unique_ptr<unsigned long[]> mantissa;
-   long size;
+//private:
+
+   void multiply_mantissa_by_ui(uint32_t value) {
+      // multiply from msb to lsb so we don't multiply the carries
+      for (size_t i = buffer_length; 0 < i; --i) {
+         multiply_mantissa_at_index_by_ui(i - 1, value);
+      }
+   }
+
+   void multiply_mantissa_at_index_by_ui(size_t index, uint32_t value) {
+      uint64_t result = static_cast<uint64_t>(mantissa[index]) * value;
+      mantissa[index] = result & 0xFFFFFFFF;
+      add_to_mantissa_at_index_ui(index + 1, result >> 32);
+   }
+
+   void add_to_mantissa_at_index_ui(size_t index, uint32_t value) {
+      if (buffer_length < index) throw nullptr;
+      if (buffer_length == index) {
+         allocate_at_least(buffer_length + 1);
+      }
+      uint32_t carry;
+      mantissa[index] = __builtin_addc(mantissa[index], value, 0, &carry);
+      if (0 < carry) {
+         add_to_mantissa_at_index_ui(index + 1, carry);
+      }
+   }
+
+   void allocate_at_least(size_t requested_length) {
+      // Do nothing if requested_length <= buffer_length
+      if (buffer_length < requested_length) {
+         auto temp = std::make_unique<uint32_t[]>(requested_length);
+         if (mantissa) {
+            for (size_t i = 0; i < buffer_length; ++i) {
+               temp[i] = mantissa[i];
+            }
+         }
+         buffer_length = requested_length;
+         mantissa.swap(temp);
+      }
+   }
+
+   std::unique_ptr<uint32_t[]> mantissa;
+   size_t buffer_length; // Tracks the length of the allocated buffer
+   size_t mantissa_length; // Tracks index of the most significant bit
+   //bool is_negative;
 };
 
 } // namespace tbl
