0ed074f54410199c425607206a00d906c576d11e
Brandon Lyons <t.brandon.lyons@gmail.com>
2016-11-15 06:13:49 -0500

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "doctest/doctest.h"
 
 #include <algorithm>
+#include <atomic>
 #include <chrono>
 #include <iostream>
 
@@ -93,17 +94,15 @@ struct table_t {
    using container_type = std::vector<TYPE>;
    using size_type = typename std::vector<TYPE>::size_type;
 
-   identifier_type insert(TYPE entry) {
-      size_type pos = m_entries.size();
-      m_entries.push_back(entry);
-      return identifier_type(pos);
+   void insert(TYPE entry) {
+      auto pos = std::upper_bound(m_entries.cbegin(), m_entries.cend(), entry);
+      m_entries.insert(pos, entry);
    }
 
-   TYPE remove(identifier_type id) {
-      size_type index = static_cast<size_type>(id);
+   void remove(identifier_type id) {
+      auto pos = std::upper_bound(m_entries.cbegin(), m_entries.cend(), id, [](const identifier_type& pid, const TYPE& item) { return pid < item.id(); });
       TYPE result = m_entries.at(index);
-      m_entries.erase(m_entries.begin() + index);
-      return result;
+      m_entries.erase(pos);
    }
 
    size_type size() const {
@@ -124,7 +123,8 @@ enum class account_type_t {
 struct account_t {
 
    account_t(std::string name, account_type_t type)
-      : m_name(name)
+      : m_id(++max_id)
+      , m_name(name)
       , m_type(type) {}
 
    const std::string& name() const {
@@ -135,14 +135,22 @@ struct account_t {
       return m_type;
    }
 
+   identifier_t<account_t> id() const {
+      return m_id;
+   }
+
    bool operator<(const account_t& other) const {
       return (m_name < other.m_name);
    }
 
 private:
+   identifier_t<account_t> m_id;
    std::string m_name;
    account_type_t m_type;
+   static std::atomic_size_t max_id;
 }; // struct account_t
+
+std::atomic_size_t account_t::max_id{0};
 
 struct transaction_t {
 
@@ -158,6 +166,10 @@ struct transaction_t {
       return m_date;
    }
 
+   identifier_t<transaction_t> id() const {
+      return m_id;
+   }
+
    bool operator<(const transaction_t& other) const noexcept {
       return (m_id < other.m_id);
    }
@@ -166,24 +178,33 @@ private:
    identifier_t<transaction_t> m_id;
    std::string m_date;
    std::string m_memo;
-   static std::size_t max_id;
+   static std::atomic_size_t max_id;
 }; // struct transaction_t
 
-std::size_t transaction_t::max_id = 0;
+std::atomic_size_t transaction_t::max_id{0};
 
 struct adjustment_t {
    using transaction_id_t = table_t<transaction_t>::identifier_type;
    using account_id_t = table_t<account_t>::identifier_type;
 
    adjustment_t(transaction_id_t transaction_id, account_id_t account_id)
-      : m_transaction_id(transaction_id)
+      : m_id(++max_id)
+      , m_transaction_id(transaction_id)
       , m_account_id(account_id) {}
 
+   identifier_t<adjustment_t> id() const {
+      return m_id;
+   }
+
 private:
+   identifier_t<adjustment_t> m_id;
    transaction_id_t m_transaction_id;
    account_id_t m_account_id;
    std::string m_amount;
+   static std::atomic_size_t max_id;
 }; // struct adjustment_t
+
+std::atomic_size_t adjustment_t::max_id{0};
 
 struct ledger_t {
    table_t<transaction_t> transaction_table;
@@ -203,17 +224,17 @@ extern "C" int main(int argc, const char* argv[]) {
 
    ledger_t ledger;
    ledger.account_table.insert({"Starting Balance", account_type_t::UNBUDGETED_ACCOUNT});
-   auto aid001 = ledger.account_table.insert({"Checking", account_type_t::BUDGETED_ACCOUNT});
+   ledger.account_table.insert({"Checking", account_type_t::BUDGETED_ACCOUNT});
    ledger.account_table.insert({"Savings", account_type_t::BUDGETED_ACCOUNT});
    ledger.account_table.insert({"Groceries", account_type_t::BUDGET_CATEGORY});
    ledger.account_table.insert({"Wegmans", account_type_t::INCOME_EXPENSE});
    ledger.account_table.insert({"Walmart", account_type_t::INCOME_EXPENSE});
-   auto tid001 = ledger.transaction_table.insert({"2016-11-14"});
-   ledger.adjustment_table.insert({tid001, aid001});
+   ledger.transaction_table.insert({"2016-11-14"});
+   //ledger.adjustment_table.insert({tid001, aid001});
 
-   auto tid = ledger.transaction_table.insert({ "1999-12-31" });
-   auto aid = ledger.account_table.insert({ "Checking", account_type_t::INCOME_EXPENSE });
-   ledger.adjustment_table.insert({ tid, aid });
+   ledger.transaction_table.insert({ "1999-12-31" });
+   ledger.account_table.insert({ "Checking", account_type_t::INCOME_EXPENSE });
+   //ledger.adjustment_table.insert({ tid, aid });
    std::cout << ledger.adjustment_table.size() << std::endl;
    return unit_test_results; // the result from doctest is propagated here as well
 }
