d5f85b25763c905ebaba8bda5cbc0fc9bcffcc75
Brandon Lyons <t.brandon.lyons@gmail.com>
2019-01-15 16:50:03 -0500

    Added file to declare money class

---
+++ src/money/money.hpp
@@ -0,0 +1,9 @@
+
+money operator+(money, money)
+money operator-(money, money)
+money operator*(money, number)
+money operator*(number, money)
+money operator%(money, number)
+money operator%(number, money)
+money operator/(money, number)
+number operator/(money, money)
