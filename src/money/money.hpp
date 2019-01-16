7132f2ec9915148981f4ec76fd19248ae3cfb846
Brandon Lyons <t.brandon.lyons@gmail.com>
2019-01-16 16:35:44 -0500

    Initial implementation for money_t

--- src/money/money.hpp
+++ src/money/money.hpp
@@ -14,45 +14,95 @@ PERFORMANCE OF THIS SOFTWARE.
 ******************************************************************************/
 #ifndef COCLES_MONEY_MONEY_HPP
 #define COCLES_MONEY_MONEY_HPP
+///////////////////////////////////////////////////////////////////////////////
+// Defines class to represent money
+//
+// Only represent up to 10,000,000,000,000,000.00
+//
+// Use cases:
+// - Sum up amounts from each adjustment for a specific account
+// - Convert to a string for display
+///////////////////////////////////////////////////////////////////////////////
+
+#include <string>
 
 namespace money {
 
 struct money_t {
 
-   money_t(void);
+   money_t(void)
+      : m_value(0) { }
 
-   explicit money_t(const std::string new_value);
+   explicit money_t(std::string const& value)
+      : m_value(0)
+   {
+      int64_t whole, frac;
+      int result = sscanf(new_value.c_str(), "%ld.%2ld", &whole, &frac);
+      if (result > 0) {
+         value = whole * 100;
+         if (result > 1) {
+            value += frac;
+         }
+      }
+   }
 
-   money_t(money_t const& other);
+   money_t(money_t const& other)
+      : m_value(other.m_value) { }
 
-   std::string to_string(void) const;
+   std::string to_string(void) const {
+      constexpr int BUF_LENGTH = 24;
+      int64_t whole = value / 100;
+      int64_t fract = value / 100;
+      char buffer[BUF_LENGTH];
+      std::snprintf(buffer, BUF_LENGTH, "%ld.%02ld", whole, frac);
+      return std::string(buffer);
+   }
 
-   money_t& operator = (money_t other);
+   money_t& operator = (money_t other) {
+      swap(*this, other);
+      return *this;
+   }
 
-   money_t& operator == (money_t const& other) const;
+   money_t& operator == (money_t const& other) const {
+      return m_value == other.m_value;
+   }
 
-   money_t& operator += (money_t const& other);
+   money_t& operator += (money_t const& other) {
+      m_value += other.m_value;
+      return *this;
+   }
 
-   money_t& operator -= (money_t const& other);
+   money_t& operator -= (money_t const& other) {
+      m_value -= other.m_value;
+      return *this;
+   }
 
-   money_t& operator %= (money_t const& other);
+   //money_t& operator %= (money_t const& other);
+   //money_t& operator /= (money_t const& other);
 
-   money_t& operator /= (money_t const& other);
+   money_t& operator *= (int64_t other) {
+      m_value *= other;
+      return *this;
+   }
+   money_t& operator %= (int64_t other) {
+      m_value %= other;
+      return *this;
+   }
+   money_t& operator /= (int64_t other) {
+      m_value /= other;
+      return *this;
+   }
+
+   friend void swap(money_t& first, money_t& second)
+   {
+      using std::swap;
+      swap(first.m_value, second.m_value);
+   }
 
 private:
-   int64_t value;
+   int64_t m_value;
 }; // struct money_t
-
-money_t operator + (money_t, money_t);
-money_t operator - (money_t, money_t);
-money_t operator * (money_t, int);
-money_t operator * (int, money_t); // Can't implement in class
-money_t operator % (money_t, int);
-money_t operator % (money_t, money_t);
-money_t operator / (money_t, int);
-int operator / (money_t, money_t);
 
 } // namespace money
 
 #endif // COCLES_MONEY_MONEY_HPP
-
