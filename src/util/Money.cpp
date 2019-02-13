bcf3dabc37ec68d563c325d447e19516f6eb37ba
Brandon Lyons <t.brandon.lyons@gmail.com>
2019-02-13 15:33:32 -0500

    cleaned up Money class

--- src/util/Money.cpp
+++ src/util/Money.cpp
@@ -1,23 +1,89 @@
-/*****************************************************************************
-ISC License
-Copyright (c) 2016, Timothy Lyons
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-******************************************************************************
-Defines class to represent money
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* ISC License
+*
+* Copyright (c) 2019, Timothy Lyons
+*
+* Permission to use, copy, modify, and/or distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
+*
+* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+* REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+* AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+* INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+* LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+* OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+* PERFORMANCE OF THIS SOFTWARE.
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#include "Money.hpp"
 
-Only represent up to 10,000,000,000,000,000.00
+#include <iomanip>
+#include <sstream>
 
-Use cases:
-- Sum up amounts from each adjustment for a specific account
-- Convert to a string for display
-******************************************************************************/
+namespace util {
+
+Money::Money(void)
+   : m_value(0) { }
+
+Money::Money(std::string const& new_value)
+   : m_value(0)
+{
+   long double temp;
+   std::istringstream value_stream(new_value);
+   value_stream >> std::get_money(temp);
+   m_value = temp;
+}
+
+Money::Money(Money const& other)
+   : m_value(other.m_value) { }
+
+std::string Money::to_string(void) const {
+   std::ostringstream value_stream;
+   value_stream << std::put_money(m_value);
+   return value_stream.str();
+}
+
+Money& Money::operator = (Money other) {
+   swap(*this, other);
+   return *this;
+}
+
+bool Money::operator == (Money const& other) const {
+   return m_value == other.m_value;
+}
+
+Money& Money::operator += (Money const& other) {
+   m_value += other.m_value;
+   return *this;
+}
+
+Money& Money::operator -= (Money const& other) {
+   m_value -= other.m_value;
+   return *this;
+}
+
+//Money& operator %= (Money const& other);
+//Money& operator /= (Money const& other);
+
+Money& Money::operator *= (int64_t other) {
+   m_value *= other;
+   return *this;
+}
+
+Money& Money::operator %= (int64_t other) {
+   m_value %= other;
+   return *this;
+}
+
+Money& Money::operator /= (int64_t other) {
+   m_value /= other;
+   return *this;
+}
+
+void swap(Money& first, Money& second) {
+   using std::swap;
+   swap(first.m_value, second.m_value);
+}
+
+} // namespace util
--- src/util/Money.hpp
+++ src/util/Money.hpp
@@ -1,19 +1,24 @@
-/*****************************************************************************
-ISC License
-Copyright (c) 2016, Timothy Lyons
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-******************************************************************************/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* ISC License
+*
+* Copyright (c) 2019, Timothy Lyons
+*
+* Permission to use, copy, modify, and/or distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
+*
+* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+* REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+* AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+* INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+* LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+* OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+* PERFORMANCE OF THIS SOFTWARE.
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef COCLES_UTIL_MONEY_HPP
 #define COCLES_UTIL_MONEY_HPP
+
 ///////////////////////////////////////////////////////////////////////////////
 // Defines class to represent money
 //
@@ -28,80 +33,37 @@ PERFORMANCE OF THIS SOFTWARE.
 
 namespace util {
 
-struct money_t {
+struct Money {
 
-   money_t(void)
-      : m_value(0) { }
+   Money(void);
 
-   explicit money_t(std::string const& value)
-      : m_value(0)
-   {
-      int64_t whole, frac;
-      int result = sscanf(new_value.c_str(), "%ld.%2ld", &whole, &frac);
-      if (result > 0) {
-         value = whole * 100;
-         if (result > 1) {
-            value += frac;
-         }
-      }
-   }
+   explicit Money(std::string const& value);
 
-   money_t(money_t const& other)
-      : m_value(other.m_value) { }
+   Money(Money const& other);
 
-   std::string to_string(void) const {
-      constexpr int BUF_LENGTH = 24;
-      int64_t whole = value / 100;
-      int64_t fract = value / 100;
-      char buffer[BUF_LENGTH];
-      std::snprintf(buffer, BUF_LENGTH, "%ld.%02ld", whole, frac);
-      return std::string(buffer);
-   }
+   std::string to_string(void) const;
 
-   money_t& operator = (money_t other) {
-      swap(*this, other);
-      return *this;
-   }
+   Money& operator = (Money other);
 
-   money_t& operator == (money_t const& other) const {
-      return m_value == other.m_value;
-   }
+   bool operator == (Money const& other) const;
 
-   money_t& operator += (money_t const& other) {
-      m_value += other.m_value;
-      return *this;
-   }
+   Money& operator += (Money const& other);
 
-   money_t& operator -= (money_t const& other) {
-      m_value -= other.m_value;
-      return *this;
-   }
+   Money& operator -= (Money const& other);
 
-   //money_t& operator %= (money_t const& other);
-   //money_t& operator /= (money_t const& other);
+   //Money& operator %= (Money const& other);
+   //Money& operator /= (Money const& other);
 
-   money_t& operator *= (int64_t other) {
-      m_value *= other;
-      return *this;
-   }
-   money_t& operator %= (int64_t other) {
-      m_value %= other;
-      return *this;
-   }
-   money_t& operator /= (int64_t other) {
-      m_value /= other;
-      return *this;
-   }
+   Money& operator *= (int64_t other);
 
-   friend void swap(money_t& first, money_t& second)
-   {
-      using std::swap;
-      swap(first.m_value, second.m_value);
-   }
+   Money& operator %= (int64_t other);
+   Money& operator /= (int64_t other);
+
+   friend void swap(Money& first, Money& second);
 
 private:
    int64_t m_value;
-}; // struct money_t
+}; // struct Money
 
 } // namespace util
 
